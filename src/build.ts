8be789eb0575c99c8e9d713a0743e16ce84aa10a
chidelma <samuel@clickspace.com>
2025-04-12 11:45:17 -0400

    feat: added dist folder

--- src/build.ts
+++ src/build.ts
@@ -5,7 +5,8 @@ Bun.build({
         whitespace: true,
         syntax: true,
         identifiers: true,
-    }
+    },
+    naming: "yon-ui.min.js",
 }).then(() => {
     console.log("Build completed successfully!");
 }).catch((err) => {
